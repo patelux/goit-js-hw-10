791d8589e8f2a6b95297339f3d2c053f2477bdb0
Natalia Levenets <levenets.lot@gmail.com>
2023-01-12 16:11:20 +0200

    first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+// for (let i = 0; i < amount; i += 1) {
+//     createPromise(1 + i, delay + i * step)
+///////
--- src/js/script.js
+++
@@ -1,3 +0,0 @@
-// for (let i = 0; i < amount; i += 1) {
-//     createPromise(1 + i, delay + i * step)
-///////
